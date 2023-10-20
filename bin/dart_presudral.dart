d92adbc2bdba11d765de5daf108426d99d5f46e5
AbdalazizTll <abdalaziztll992010@gmail.com>
2023-10-19 23:44:42 -0700

    fix some issue

--- bin/dart_presudral.dart
+++ bin/dart_presudral.dart
@@ -1,35 +1,40 @@
 
 main() {
-  Animal cat = Animal(name: 'katy', color: 'white',position: Position(x: 2, y: 2));
-  print(cat.position.y);
-  // cat.jump(20);
-  print(cat.position.y);
+print(DateTime.now()); 
 
-  Animal cat1 =Animal(name: 'katy', color: 'white', position: Position(x: 2, y: 2));
-
-  if(cat==cat1){
-    print('Match');
-  }
-  else {
-    print('Not Match');
-  }
 }
+//   Animal cat = Animal(name: 'katy', color: 'white',position: Position(x: 2, y: 2));
+//   print(cat.position.y);
+//   // cat.jump(20);
+//   print(cat.position.y);
 
-class Animal {
-  String name;
-  String color;
+//   Animal cat1 =Animal(name: 'katy', color: 'white', position: Position(x: 2, y: 2));
+
+//   if(cat==cat1){
+//     print('Match');
+//   }
+//   else {
+//     print('Not Match');
+//   }
+// }
+
+// class Animal {
+//   String name;
+//   String color;
   
-  Position position;
+//   Position position;
 
-  Animal({required this.name, required this.color, required this.position});
+//   Animal({required this.name, required this.color, required this.position});
 
-  jump(int height){
-   this.position.y = this.position.y + height;
-  }
-}
+//   jump(int height){
+//    this.position.y = this.position.y + height;
+//   }
+// }
 
-class Position {
-  int x;
-  int y;
-  Position({required this.x, required this.y});
-}
+
+// class Position {
+//   int x;
+//   int y;
+//   Position({required this.x, required this.y});
+
+// }
