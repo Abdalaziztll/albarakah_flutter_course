516dec4a2489e353ab1b115922b291a61e37cdad
AbdalazizTll <abdalaziztll992010@gmail.com>
2023-11-06 06:11:26 -0800

    first

--- lib/main.dart
+++ lib/main.dart
@@ -1,123 +1,152 @@
-import 'package:controller_types/config/router/go_route.dart';
-import 'package:controller_types/model/cart_model.dart';
-import 'package:controller_types/service/cartd_service.dart';
-import 'package:controller_types/service/graphql/graphql.dart';
-import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
-import 'package:graphql_flutter/graphql_flutter.dart';
+import 'dart:math';
 
-main() async{
-  await initHiveForFlutter();
-  
- await getData();
+import 'package:flutter/material.dart';
+
+main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp.router(
-      routerConfig: router,
+  Widget build(BuildContext contes) {
+    return MaterialApp(
+      home: CustomizePage(),
     );
   }
 }
 
-class HomePage extends StatefulWidget {
-  const HomePage({super.key});
+class BasePage extends StatelessWidget {
+  const BasePage({
+    super.key,
+  });
 
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
-        future: CardService().getCarts(),
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            dynamic temp = snapshot.data;
-            CardModel cards = CardModel.fromMap(temp.data);
-
-            return DefaultTabController(
-              length: cards.Carts.length,
-              child: Scaffold(
-                appBar: AppBar(
-                  title: Text('TabBar Example'),
-                  bottom: TabBar(
-                      tabs: List.generate(
-                          cards.Carts.length,
-                          (index) => Tab(
-                                icon: Icon(Icons.leaderboard),
-                              ))),
-                ),
-                body: TabBarView(
-                  children:
-                      List.generate(cards.Carts.length, (index) => Scaffold()),
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            InkWell(
+              onTap: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => HomePage(),
+                    ));
+              },
+              child: Container(
+                width: 100,
+                height: 100,
+                color: Colors.blue,
+                child: Hero(
+                  tag: 'anime',
+                  child: Center(child: Text('Hello world')),
                 ),
               ),
-            );
-          } else {
-            return Center(
-              child: CircularProgressIndicator(),
-            );
-          }
-        });
-  }
-}
-
-class HomePage1 extends StatefulWidget {
-  const HomePage1({super.key});
-
-  @override
-  State<HomePage1> createState() => _HomePageState1();
-}
-
-class _HomePageState1 extends State<HomePage1> {
-  @override
-  Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 3,
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text('TabBar Example'),
-          bottom: TabBar(tabs: [
-            Icon(Icons.percent),
-            Icon(Icons.percent),
-            Icon(Icons.percent)
-          ]),
-        ),
-        body: TabBarView(children: [
-          Container(
-            color: Colors.red,
-          ),
-          Container(
-            color: Colors.blue,
-          ),
-          InkWell(
-            onTap: () {
-              context.go('/testpage');
-            },
-            child: Container(
-              color: Colors.green,
             ),
-          )
-        ]),
+            InkWell(
+                onTap: () {
+                  Navigator.pop(context);
+                },
+                child: Text('prevouis page')),
+            Hero(
+                tag: 'logo',
+                child: FlutterLogo(
+                  size: 200,
+                ))
+          ],
+        ),
       ),
     );
   }
 }
 
-class TestPage extends StatelessWidget {
-  const TestPage({super.key});
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
 
+class _HomePageState extends State<HomePage> {
+  bool logic = true;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: ListTile(
+        leading: Hero(
+          tag: 'logo',
+          child: FlutterLogo(),
+        ),
+        trailing: Checkbox(
+          onChanged: (value) {
+            setState(() {
+              logic = value!;
+            });
+            print(logic);
+          },
+          value: logic,
+        ),
+      ),
+    );
+  }
+}
+
+class CustomizePage extends StatefulWidget {
+  const CustomizePage({super.key});
+
+  @override
+  State<CustomizePage> createState() => _CustomizePageState();
+}
+
+class _CustomizePageState extends State<CustomizePage> {
+  double width = 200;
+  double height = 200;
+  Color color = Colors.red;
+  bool magic = true;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: FlutterLogo(),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            InkWell(
+              onTap: () {
+                print('object');
+                setState(() {
+                  width = width + 30;
+                  height = height + 30;
+                  color = Colors.blue;
+                });
+              },
+              child: Visibility(
+                replacement: FlutterLogo(
+                  size: 300,
+                  duration: Duration(seconds: 4),
+                ),
+                visible: magic,
+                child: AnimatedContainer(
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: color,
+                  ),
+                  duration: Duration(seconds: 1),
+                  width: width,
+                  height: height,
+                ),
+              ),
+            ),
+            InkWell(
+                onTap: () {
+                  setState(() {
+                    magic = !magic;
+                  });
+                },
+                child: Text(magic ? 'hide' : 'show'))
+          ],
+        ),
       ),
     );
   }
