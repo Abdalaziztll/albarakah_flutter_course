529e5d5e59512039121b0870332db89224c96fb9
AbdalazizTll <abdalaziztll992010@gmail.com>
2023-11-18 06:45:41 -0800

    localization

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,6 @@ import 'dart:async';
 import 'package:another_flushbar/flushbar.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
-import 'package:intro_to_uitity/componenet/flushbar.dart';
-import 'package:intro_to_uitity/model/quiz_model.dart';
 
 main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -20,45 +18,34 @@ main() async {
   );
 }
 
-List<QuestionModel> questions = [
-  QuestionModel(question: 'What is My Name ?', availableTime: 20, answers: [
-    AnswerModel(answer: 'Ahmad', isCorrect: false),
-    AnswerModel(answer: 'Tareq', isCorrect: false),
-    AnswerModel(answer: 'Noor', isCorrect: false),
-    AnswerModel(answer: 'Abd', isCorrect: true),
-  ]),
-  QuestionModel(question: 'What is My Name ?', availableTime: 20, answers: [
-    AnswerModel(answer: 'Ahmad', isCorrect: false),
-    AnswerModel(answer: 'Tareq', isCorrect: false),
-    AnswerModel(answer: 'Noor', isCorrect: false),
-    AnswerModel(answer: 'Noor', isCorrect: false),
-    AnswerModel(answer: 'Abd', isCorrect: true),
-  ]),
-  QuestionModel(question: 'What is My Name ?', availableTime: 20, answers: [
-    AnswerModel(answer: 'Ahmad', isCorrect: false),
-    AnswerModel(answer: 'Tareq', isCorrect: false),
-    AnswerModel(answer: 'Noor', isCorrect: false),
-    AnswerModel(answer: 'Abd', isCorrect: true),
-  ])
-];
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(useMaterial3: true, colorSchemeSeed: Colors.purple),
-      debugShowCheckedModeBanner: false,
       localizationsDelegates: context.localizationDelegates,
       supportedLocales: context.supportedLocales,
       locale: context.locale,
-      home: QuizApp(),
+      home: HomePage(),
     );
   }
 }
 
-bool value = true;
+List<String> names = [
+  'Rabee',
+  "Mosab",
+  "Nadim",
+  "Faris",
+  "Kinan",
+  "Omar",
+  "Madeh",
+  "Alaa",
+  "Abdullah",
+  "Karm",
+  "Majd",
+  "Mhd"
+];
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -67,137 +54,88 @@ class HomePage extends StatefulWidget {
   State<HomePage> createState() => _HomePageState();
 }
 
+int count = 1;
+
 class _HomePageState extends State<HomePage> {
+  late List<TextEditingController> controller;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Hello World').tr(),
+        title: Text('Hello world').tr(),
         actions: [
-          Text(value ? 'en' : 'ar'),
-          Switch(
-              value: value,
-              onChanged: (val) {
-                if (val) {
+          ElevatedButton(
+              onPressed: () {
+                if (context.locale.languageCode == 'ar') {
                   context.setLocale(Locale('en'));
-                  setState(() {
-                    value = val;
-                  });
                 } else {
-                  setState(() {
-                    value = val;
-                  });
                   context.setLocale(Locale('ar'));
                 }
-              })
+              },
+              child: Text('translate').tr())
         ],
       ),
-      body: Center(
-        child: InkWell(
-            onTap: () {
-              // showMyNewSnackBar(context);
-            },
-            child: Text('This is an Example').tr()),
-      ),
-    );
-  }
-}
 
-PageController controller = PageController();
-int score = 0;
-num timer = 0;
-
-class QuizApp extends StatefulWidget {
-  const QuizApp({super.key});
-
-  @override
-  State<QuizApp> createState() => _QuizAppState();
-}
-
-class _QuizAppState extends State<QuizApp> {
-  @override
-  void initState() {
-    Timer.periodic(Duration(seconds: 1), (t) {
-      setState(() {
-        timer++;
-      });
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: PageView.builder(
-        physics: NeverScrollableScrollPhysics(),
-        controller: controller,
-        itemCount: questions.length,
-        itemBuilder: (context, index) => Scaffold(
-          appBar: AppBar(
-            actions: [Text(score.toString())],
-            centerTitle: true,
-            title: Text((index + 1).toString()),
-          ),
-          body: Column(
-            children: [
-              ListTile(
-                title: Text(questions[index].question),
-              ),
-              SizedBox(
-                height: 500,
-                child: ListView.builder(
-                  itemCount: questions[index].answers.length,
-                  itemBuilder: (context, ind) => InkWell(
-                    onTap: () {
-                      checkAnswer(
-                          isCorrect: questions[index].answers[ind].isCorrect,
-                          context: context,
-                          answerTime: timer,
-                          availableTime: questions[index].availableTime);
-                      timer = 0;
-                    },
-                    child: ListTile(
-                      leading: CircleAvatar(
-                        child: Text((ind + 1).toString()),
+      body: Column(
+        children: [
+          SizedBox(
+            height: 500,
+            child: ListView.builder(
+                itemCount: count,
+                itemBuilder: (context, index) {
+                  controller =
+                      List.generate(count, (index) => TextEditingController());
+                  return Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: SizedBox(
+                      width: 300,
+                      child: TextField(
+                        controller: controller[index],
+                        decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            hintText: (index + 1).toString()),
                       ),
-                      title: Text(questions[index].answers[ind].answer),
                     ),
-                  ),
-                ),
-              ),
-              CircularProgressIndicator(value: timer * 0.05)
-            ],
+                  );
+                }),
           ),
-        ),
+          IconButton(
+              onPressed: () {
+                setState(() {
+                  count++;
+                });
+                controller.forEach((element) {
+                  print(element.text);
+                });
+              },
+              icon: Icon(Icons.add))
+        ],
       ),
-    );
-  }
-}
+      // body: ReorderableListView.builder(
+      //   itemBuilder: (context, index) => ListTile(
+      //     key: GlobalKey(),
+      //     title: Text(names[index]),
+      //   ),
+      //   itemCount: names.length,
+      //   onReorder: (oldIndex, newIndex) {
+      //     // String temp = names[oldIndex];
+      //     // if (oldIndex < newIndex) {
+      //     //   names.removeAt(oldIndex);
+      //     //   names.insert(newIndex - 1, temp);
+      //     // } else {
+      //     //   names.removeAt(oldIndex);
+      //     //   names.insert(newIndex, temp);
+      //     // }
 
-checkAnswer(
-    {required bool isCorrect,
-    required BuildContext context,
-    required num availableTime,
-    required num answerTime}) {
-  if (availableTime > answerTime) {
-    if (isCorrect) {
-      ScaffoldMessenger.of(context).showSnackBar(new SnackBar(
-        content: Text('Success'),
-        backgroundColor: Colors.green,
-      ));
-      controller.nextPage(duration: Duration(seconds: 1), curve: Curves.ease);
-      score++;
-    } else {
-      ScaffoldMessenger.of(context).showSnackBar(new SnackBar(
-        content: Text('Failed'),
-        backgroundColor: Colors.red,
-      ));
-      controller.nextPage(duration: Duration(seconds: 1), curve: Curves.ease);
-      score--;
-    }
-  } else {
-    ScaffoldMessenger.of(context).showSnackBar(new SnackBar(
-      content: Text('Time out'),
-      backgroundColor: Colors.blue,
-    ));
+      //     if (oldIndex < newIndex) {
+      //       // removing the item at oldIndex will shorten the list by 1.
+      //       newIndex -= 1;
+      //     }
+      //     String temp = names.removeAt(oldIndex);
+      //     names.insert(newIndex, temp);
+      //   },
+      // ),
+    );
   }
 }
